e3672751d71abdb68d311499c954fdacb50e77e5
Quentin Vespero <quentinvespero@gmail.com>
2025-02-03 01:59:45 +0100

    preparing the project

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+const AppRouter = () => {
+    <BrowserRouter>
+        <Routes>
+            <Route/>
+        </Routes>
+    </BrowserRouter>
+}
+
+export default AppRouter
