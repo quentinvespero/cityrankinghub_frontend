d7dcd1443deab1c61c13271952f5cc9783fea284
Quentin Vespero <quentinvespero@gmail.com>
2025-02-13 01:48:57 +0100

    added component MenuBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import MenuBar from "./components/menuBar/MenuBar"
+
 const App = () => {
     return (
         <div className="app">
-            
+            <MenuBar/>
         </div>
     )
 }
